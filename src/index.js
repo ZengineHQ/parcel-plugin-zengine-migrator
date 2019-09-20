1daee4c2f44b5b80857f88de11bce49652c9d833
tehpsalmist <bstewardcodes@gmail.com>
2019-09-20 03:07:54 -0400

    feat: watch .legacy-output files and rebuild appropriate asset types

--- src/index.js
+++ src/index.js
@@ -6,4 +6,24 @@ module.exports = (bundler) => {
   bundler.addAssetType('.css', require.resolve('./CSSAsset.js'))
   bundler.addAssetType('.html', require.resolve('./HTMLAsset.js'))
 
+  // Setup watcher for the source files
+  bundler.on('bundled', bundle => {
+    const pluginHTML = path.resolve(process.cwd(), '.legacy-output', 'plugin.html')
+    const pluginCSS = path.resolve(process.cwd(), '.legacy-output', 'plugin.css')
+    const pluginJS = path.resolve(process.cwd(), '.legacy-output', 'plugin.js')
+
+    for (const childBundle of bundle.childBundles) {
+      if (childBundle.type === 'css' && fs.existsSync(pluginCSS)) {
+        bundler.watch(pluginCSS, childBundle.entryAsset)
+      }
+
+      if (childBundle.type === 'js' && fs.existsSync(pluginJS)) {
+        bundler.watch(pluginJS, childBundle.entryAsset)
+      }
+    }
+
+    if (fs.existsSync(pluginHTML)) {
+      bundler.watch(pluginHTML, bundler.mainBundle.entryAsset)
+    }
+  })
 }
