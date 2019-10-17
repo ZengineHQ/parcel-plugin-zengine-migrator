1c26bf173f2449a443b9da9cdc5efe0bc11fdc7e
tehpsalmist <bstewardcodes@gmail.com>
2019-10-17 00:49:53 -0400

    get actual namespace from maya.json

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 
 const promisify = function (fn) {
   return function (...args) {
@@ -22,8 +23,20 @@ const wgnTransformer = (contents, namespace) => contents
   .replace(/wgn([A-Za-z])/g, (match, first) => `${kebabToCamelCase(namespace)}${first.toUpperCase()}`)
   .replace(/wgn/g, () => namespace)
 
+const mayaJSON = fs.existsSync(relCwd('..', '..', 'maya.json')) && require(relCwd('..', '..', 'maya.json'))
+
+const pluginName = path.basename(process.cwd())
+
+if (mayaJSON) {
+  try {
+    mayaJSON.environments[process.env.ZENGINE_ENV].plugins[pluginName].namespace.toUpperCase()
+  } catch (e) {
+    console.error(`malformed maya.json:`, e)
+  }
+}
+
 const getNamespace = () => {
-  return 'letter-generator'
+  return mayaJSON ? mayaJSON.environments[process.env.ZENGINE_ENV].plugins[pluginName].namespace : 'default'
 }
 
 module.exports = {
